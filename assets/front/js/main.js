1c0a58260bda4c8700e5fa117fd7415d4188fb9d
assabr <assabr.org@gmail.com>
2020-11-24 13:17:15 +0100

    home page v1 finished

--- assets/front/js/main.js
+++ assets/front/js/main.js
@@ -1,55 +1,130 @@
 (() => {
-  const navbar = document.querySelector('.nav');
-  const sticky = navbar.offsetTop;
-  const width = 576;
+  /**
+   * Query elements
+   */
+  const width = 600,
+    isActive = 'is__active';
+
+  /**
+   * Adding scroll class to nav
+   */
+  const header = document.querySelector('.header'),
+    sticky = header.offsetTop;
   const navbarScroll = () => {
-    if (window.pageYOffset > sticky && window.innerWidth >= width) {
-      navbar.classList.add('nav_scroll');
+    if (window.pageYOffset > sticky) {
+      header.classList.add('header_scroll');
     } else {
-      navbar.classList.remove('nav_scroll');
+      header.classList.remove('header_scroll');
     }
   };
+  document.addEventListener('scroll', navbarScroll);
 
-  window.onscroll = navbarScroll;
+  /**
+   * Menu
+   */
+  const burger = document.querySelector('.burger'),
+    menu = document.querySelector('.nav__menu'),
+    submenu = document.querySelector('.has__submenu');
 
-
-  
-  // Adding/Removing class to navbar section on scroll
-  // window.addEventListener('scroll', () => {
-
-  //     const scrollable = 100;
-  //     const scrolled = window.scrollY;
-
-  //     console.log(scrolled);
-  //     const navSec = document.getElementById('nav__section');
-
-  //     if (scrolled > scrollable) {
-  //         navSec.classList.add('has__scroll_nav');
-  //     } else {
-
-  //         navSec.classList.remove('has__scroll_nav');
-  //     }
-  // });
-
-  //Burger
-  const burger = document.querySelector('.burger');
-  const navItems = document.querySelector('.navbar__menu');
   burger.addEventListener('click', () => {
     burger.classList.toggle('burger_active');
-    navItems.classList.toggle('is__active');
+    menu.classList.toggle('nav__menu_active');
   });
 
-  // Open search form
-  const searchBtn = document.querySelector('#search-btn');
-  const searchForm = document.querySelector('#search-form');
-  const closeForm = document.querySelector('#search-close');
-
-  searchBtn.addEventListener('click', () => {
-    searchForm.classList.toggle('open');
-    // searchBtn.classList.toggle('open');
-    return false;
+  // Removing active classes from burger and menu
+  // if window is more than 600px
+  window.addEventListener('resize', () => {
+    if (window.innerWidth > width) {
+      burger.classList.remove('burger_active');
+      menu.classList.remove('nav__menu_active');
+    }
   });
 
-  // Close search form
+  /**
+   * Submenu
+   */
+  window.addEventListener('click', (e) => {
+    let target = e.target;
 
+    do {
+      if (target == submenu && window.innerWidth < width) {
+        submenu.classList.toggle('has__submenu_active');
+        return;
+      }
+      target = target.parentNode;
+    } while (target);
+    submenu.classList.remove('has__submenu_active');
+  });
+
+  /**
+   * Search Modal
+   */
+  const openEls = document.querySelectorAll('[data-open]'),
+    closeEls = document.querySelectorAll('[data-close]'),
+    isVisible = 'is__visible';
+
+  for (const el of openEls) {
+    el.addEventListener('click', function () {
+      const modalId = this.dataset.open;
+      document.getElementById(modalId).classList.add(isVisible);
+    });
+  }
+
+  for (const el of closeEls) {
+    el.addEventListener('click', function () {
+      this.parentElement.parentElement.parentElement.classList.remove(
+        isVisible
+      );
+    });
+  }
+
+  // If click outside of modal
+  document.addEventListener('click', (e) => {
+    if (e.target == document.querySelector('.nav__search__modal.is__visible')) {
+      document
+        .querySelector('.nav__search__modal.is__visible')
+        .classList.remove(isVisible);
+    }
+  });
+
+  document.addEventListener('keyup', (e) => {
+    // If we press the ESC
+    if (
+      e.key == 'Escape' &&
+      document.querySelector('.nav__search__modal.is__visible')
+    ) {
+      document
+        .querySelector('.nav__search__modal.is__visible')
+        .classList.remove(isVisible);
+    }
+  });
+
+  /**
+   * Scroll to top btn
+   */
+  const scrollToTopBtn = document.querySelector('.scroll__toTop__btn'),
+    rootElement = document.documentElement,
+    showBtnAt = 0.3;
+
+  function handleScroll() {
+    // do something on scroll
+    var scrollTotal = rootElement.scrollHeight - rootElement.clientHeight;
+    if (rootElement.scrollTop / scrollTotal > showBtnAt) {
+      //show button
+      scrollToTopBtn.classList.add('show__btn');
+    } else {
+      //hide button
+      scrollToTopBtn.classList.remove('show__btn');
+    }
+  }
+
+  function scrollToTop() {
+    //scroll to top logic
+    rootElement.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  }
+  scrollToTopBtn.addEventListener('click', scrollToTop);
+  document.addEventListener('scroll', handleScroll);
 })();
